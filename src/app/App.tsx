9937ee14059fc493767cbb2daa7519f762f05a7a
ali <ibrakhimov7747a@gmail.com>
2024-03-28 17:15:49 +0900

    fix: orginiz header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import { About } from './screens/About';
-import { Users } from './screens/Users';
+import { Link, Route, Switch, useLocation } from 'react-router-dom';
 import {HomePage } from './screens/homePage';
 import { ProductsPage } from './screens/productsPage';
 import { OrderPage } from './screens/orderPage';
 import { UserPage } from './screens/userPage';
+import { HomeNavbar } from './components/headers/HomeNavbar';
+import { OtherNavbar } from './components/headers/OtherNavbar';
+import { Footer } from './components/footer';
 
 function App() {
+  const location = useLocation();
+  console.log(location)
   return (
-    <div>
-      <nav>
-        <ul>
-        <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/products">ProductsPage</Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li> 
-          
-        </ul>
-      </nav>
-
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
+    <>
+    {location.pathname === "/" ? <HomeNavbar/> : <OtherNavbar/>} 
       <Switch>
         <Route path="/products">
           <ProductsPage />
@@ -44,7 +28,9 @@ function App() {
           <HomePage/>
         </Route>
       </Switch>
-    </div>
+      <Footer/>
+    </>
+
   )
 }
 export default App;
