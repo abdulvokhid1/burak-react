106303e6e9910fae4acadc5a3a5a2556bc9b797c
ali <ibrakhimov7747a@gmail.com>
2024-03-30 11:32:28 +0900

    fix: homeNavbar menu part refactoring

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -2,47 +2,45 @@ import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 export function HomeNavbar() {
-    const authMember = null; 
+    const authMember = null;
     return <div className="home-navbar">
-        <Container sx={{mt:"55px", height:"642px"}}>
-            <Stack sx={{height:"50px"}} 
-            flexDirection={"row"}
-             justifyContent={"space-between"}
-              alignItems={"center"}>
+        <Container className="navbar-container">
+            <Stack className="menu">
                 <Box>
-                    <NavLink to={'/'}>
-                        <img style={{ width:'125px', height:"30px"}} src="/icons/burak.svg" />
+                    <NavLink to={'/'}> 
+                        <img className='brand-logo' src="/icons/burak.svg" />
                     </NavLink>
                 </Box>
-                <Stack flexDirection={"row"}
-                 justifyContent={"space-between"} 
-                 minWidth={"700px"} 
-                 alignItems={"center"}>
-                <Box className = {"hover-line"} >
-                    <NavLink to={'/'} activeClassName={"underline"}  >Home</NavLink>
-                </Box>
-                <Box className = {"hover-line"}>
-                    <NavLink to={'/products'} activeClassName={"underline"}  >Products</NavLink>
-                </Box >
-                {authMember ? (
-                    <Box className = {"hover-line"}>
-                    <NavLink to={'/orders'} activeClassName={"underline"}>Orders</NavLink>
-                </Box>
-                ) : null}
-                {authMember ? (
-                    <Box className = {"hover-line"}>
-                    <NavLink to={'/member-page'} activeClassName={"underline"} >My Page</NavLink>
-                </Box>
-                ) : null}
-                <Box className = {"hover-line"}>
-                    <NavLink to={'/help'} activeClassName={"underline"} >Help</NavLink>
-                </Box>
-                {!authMember ? (
-                    <Box>
-                        <Button variant="contained" style={{background:"#3776CC", color:"#f8f8ff"}}>Login</Button>
-                </Box>
-                ) : null}
-            </Stack>
+                <Stack className="links">
+                    <Box className={"hover-line"} >
+                        <NavLink to={'/'} activeClassName={"underline"}  >Home</NavLink>
+                    </Box>
+                    <Box className={"hover-line"}>
+                        <NavLink to={'/products'} activeClassName={"underline"}  >Products</NavLink>
+                    </Box >
+                    {authMember ? (
+                        <Box className={"hover-line"}>
+                            <NavLink to={'/orders'} activeClassName={"underline"}>Orders</NavLink>
+                        </Box>
+                    ) : null}
+                    {authMember ? (
+                        <Box className={"hover-line"}>
+                            <NavLink to={'/member-page'} activeClassName={"underline"} >My Page</NavLink>
+                        </Box>
+                    ) : null}
+                    <Box className={"hover-line"}>
+                        <NavLink to={'/help'} activeClassName={"underline"} >Help</NavLink>
+                    </Box>
+                    {!authMember ? (
+                        <Box>
+                            <Button variant="contained" className="login-button">Login</Button>
+                        </Box>
+                    ) : (
+                        <img className="user-avatar"
+                        src="/icons/default-user.svg"
+                        aria-haspopup = {"true"} />
+                    )}
+                </Stack>
             </Stack>
             <Stack>Detail</Stack>
         </Container>
