66b01be5cc3514b7a494382cf2b9a870d675d0bf
ali <ibrakhimov7747a@gmail.com>
2024-04-27 15:17:55 +0900

    feat: CORS integration

--- src/lib/types/product.ts
+++ src/lib/types/product.ts
@@ -12,6 +12,8 @@ export interface Product {
     productDesc?: string;
     productImages: string[];
     productViews: number;
+    createdAt: Date;
+    updatedAt: Date;
 
 }
 export interface ProductInquiry {
